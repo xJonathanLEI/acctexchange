2c07b93fc988910ac4d45664fdad1b727da961a6
Jonathan LEI <xJonathan@outlook.com>
2018-07-23 01:23:08 +0800

    Initial commit

---
+++ acctexchange.cpp
@@ -0,0 +1,217 @@
+#include "acctexchange.hpp"
+
+void acctexchange::listforsale(account_name acct, extended_asset price, account_name recipient)
+{
+    require_auth(acct);
+
+    eosio_assert(price.is_valid(), "Invalid price symbol");
+    eosio_assert(is_account(price.contract), "Token contract does not exist");
+    eosio_assert(price.amount > 0, "Price must be positive");
+
+    eosio_assert(is_account(recipient), "Recipient does not exist");
+    eosio_assert(acct != recipient, "Set a different recipient account");
+
+    /*
+     * This is to check whether acctexchange@eosio.code can pass acct@owner.
+     * However, it's possible that after calling listforsale, the permission is removed.
+     * In that case, any attempt to buy this account would fail. The admin shall forcibly remove
+     * the listing by calling removesale.
+     */
+    action(permission_level(acct, N(owner)), _self, N(assertowner), assertowner_args{acct}).send();
+
+    auto existingAcctPost = forsales.find(acct);
+    eosio_assert(existingAcctPost == forsales.end(), "Account is already for sale");
+
+    forsales.emplace(acct, [&](forsale &newSale) {
+        newSale.acct = acct;
+        newSale.price = price;
+        newSale.recipient = recipient;
+    });
+}
+
+void acctexchange::delistacct(account_name acct)
+{
+    require_auth(acct);
+
+    auto existingAcctPost = forsales.find(acct);
+    eosio_assert(existingAcctPost != forsales.end(), "Account is not for sale");
+
+    forsales.erase(existingAcctPost);
+}
+
+void acctexchange::buyacct(account_name buyer, account_name target, extended_asset price, string pub_key)
+{
+    // Auth
+
+    require_auth(buyer);
+
+    auto existingAcctPost = forsales.find(target);
+    eosio_assert(existingAcctPost != forsales.end(), "Account is not for sale");
+
+    // Protection from price-change attack
+
+    eosio_assert(existingAcctPost->price.get_extended_symbol() == price.get_extended_symbol(), "Wrong price symbol");
+    eosio_assert(existingAcctPost->price.amount <= price.amount, "Provided price is too low");
+
+    // Adjusts balance
+
+    sub_balance(buyer, existingAcctPost->price);
+
+    // Decodes public key
+
+    eosio_assert(pub_key[0] == 'E' && pub_key[1] == 'O' && pub_key[2] == 'S', "Public key must start with EOS");
+
+    string pub_key_str_raw = pub_key.substr(3);
+
+    vector<unsigned char> pub_key_decoded;
+    eosio_assert(decode_base58(pub_key_str_raw, pub_key_decoded), "Failed to decode public key");
+    eosio_assert(pub_key_decoded.size() == 37, "Invalid public key");
+
+    array<unsigned char, 33> pub_key_payload;
+    copy_n(pub_key_decoded.begin(), 33, pub_key_payload.begin());
+
+    // Reads fee settings
+
+    // Transfers fee to ADMIN
+
+    // Transfers price to recipient
+
+    action(permission_level(_self, N(active)), existingAcctPost->price.contract, N(transfer), transfer_args{_self, existingAcctPost->recipient, asset(existingAcctPost->price.amount, existingAcctPost->price.symbol), "Your account has been successfully sold!"}).send();
+
+    // Updates account owner permission
+
+    vector<key_weight> owner_keys;
+    owner_keys.push_back({public_key{0, pub_key_payload}, 1});
+    updateauth_args owner{
+        target, N(owner), 0,
+        authority{1, 0, owner_keys, vector<permission_level_weight>()}};
+
+    action(permission_level(target, N(owner)), N(eosio), N(updateauth), owner).send();
+
+    // Updates account active permission
+
+    vector<key_weight> active_keys;
+    active_keys.push_back({public_key{0, pub_key_payload}, 1});
+    updateauth_args active{
+        target, N(active), N(owner),
+        authority{1, 0, active_keys, vector<permission_level_weight>()}};
+
+    action(permission_level(target, N(owner)), N(eosio), N(updateauth), active).send();
+
+    // Erases sale post from DB
+
+    forsales.erase(existingAcctPost);
+}
+
+void acctexchange::withdraw(account_name user, extended_asset quantity)
+{
+    require_auth(user);
+
+    eosio_assert(quantity.amount > 0, "Must withdraw positive amount");
+
+    sub_balance(user, quantity);
+
+    action(permission_level(_self, N(active)), quantity.contract, N(transfer), transfer_args{_self, user, asset(quantity.amount, quantity.symbol), "Withdrawal"}).send();
+}
+
+void acctexchange::adjustfee(uint64_t new_fee)
+{
+    require_auth(ADMIN);
+}
+
+void acctexchange::removesale(account_name acct)
+{
+    require_auth(ADMIN);
+
+    auto existingAcctPost = forsales.find(acct);
+    eosio_assert(existingAcctPost != forsales.end(), "Account is not for sale");
+
+    forsales.erase(existingAcctPost);
+}
+
+void acctexchange::assertowner(account_name acct)
+{
+    require_auth2(acct, N(owner));
+}
+
+void acctexchange::handle_transfer(account_name from, account_name to, extended_asset quantity, string memo)
+{
+    // Ignores the message if it's not a transfer of CORE to this contract
+
+    if (quantity.contract != N(eosio.token) || quantity.symbol != CORE_SYMBOL)
+        return;
+    if (from == _self || to != _self)
+        return;
+
+    eosio_assert(quantity.is_valid(), "Invalid deposit token symbol");
+    eosio_assert(quantity.amount > 0, "Must transfer positive amount");
+
+    tbl_user_balances user_balances(_self, from);
+    auto existingBalance = user_balances.find(from);
+
+    if (existingBalance == user_balances.end())
+        user_balances.emplace(_self, [&](user_balance &newBalance) {
+            newBalance.user = from;
+            newBalance.balances.push_back(quantity);
+        });
+    else
+    {
+        auto balanceItem = existingBalance->balances.begin();
+        for (; balanceItem != existingBalance->balances.end(); existingBalance++)
+            if (balanceItem->get_extended_symbol() == quantity.get_extended_symbol())
+                break;
+
+        bool itemExists = balanceItem != existingBalance->balances.end();
+
+        if (itemExists)
+        {
+            eosio_assert(balanceItem->amount + quantity.amount > quantity.amount, "Balance overflow");
+            user_balances.modify(existingBalance, _self, [&](user_balance &userBalance) {
+                for (auto it = userBalance.balances.begin(); it != userBalance.balances.end(); it++)
+                    if (it->get_extended_symbol() == quantity.get_extended_symbol())
+                    {
+                        (*it) += quantity;
+                        break;
+                    }
+            });
+        }
+        else
+        {
+            user_balances.modify(existingBalance, _self, [&](user_balance &userBalance) {
+                userBalance.balances.push_back(quantity);
+            });
+        }
+    }
+}
+
+void acctexchange::sub_balance(account_name user, extended_asset quantity)
+{
+    tbl_user_balances user_balances(_self, user);
+    auto existingBalance = user_balances.find(user);
+    eosio_assert(existingBalance != user_balances.end(), "Balance object not found");
+
+    auto balanceItem = existingBalance->balances.begin();
+    for (; balanceItem != existingBalance->balances.end(); existingBalance++)
+        if (balanceItem->get_extended_symbol() == quantity.get_extended_symbol())
+            break;
+
+    eosio_assert(balanceItem != existingBalance->balances.end(), "Balance item not found");
+    eosio_assert(balanceItem->amount >= quantity.amount, "Insufficient balance");
+
+    bool erase_item = balanceItem->amount == quantity.amount;
+
+    if (erase_item && existingBalance->balances.size() == 1)
+        user_balances.erase(existingBalance);
+    else
+        user_balances.modify(existingBalance, user, [&](user_balance &userBalance) {
+            for (auto it = userBalance.balances.begin(); it != userBalance.balances.end(); it++)
+                if (it->get_extended_symbol() == quantity.get_extended_symbol())
+                {
+                    if (erase_item)
+                        userBalance.balances.erase(it);
+                    else
+                        (*it) -= quantity;
+                    break;
+                }
+        });
+}
---
+++ acctexchange.hpp
@@ -0,0 +1,143 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+
+#include <string>
+#include <limits>
+
+#include "base58.hpp"
+
+using namespace std;
+using namespace eosio;
+
+struct transfer_args
+{
+    account_name from;
+    account_name to;
+    asset quantity;
+    string memo;
+};
+
+struct assertowner_args
+{
+    account_name acct;
+};
+
+struct public_key
+{
+    uint8_t type;
+    array<unsigned char, 33> data;
+};
+
+struct permission_level_weight
+{
+    permission_level permission;
+    weight_type weight;
+};
+
+struct key_weight
+{
+    public_key key;
+    weight_type weight;
+};
+
+struct authority
+{
+    uint32_t threshold;
+    uint32_t delay_sec;
+    vector<key_weight> keys;
+    vector<permission_level_weight> accounts;
+};
+
+struct updateauth_args
+{
+    account_name account;
+    permission_name permission;
+    permission_name parent;
+    authority data;
+};
+
+class acctexchange : public contract
+{
+  public:
+    using contract::contract;
+
+    const account_name ADMIN = N(xjonathanlei);
+
+    acctexchange(account_name self) : contract(self), forsales(self, self) {}
+
+    /// @table forsales
+    struct forsale
+    {
+        account_name acct;
+        extended_asset price;
+        account_name recipient;
+
+        auto primary_key() const { return acct; }
+    };
+
+    /// @table balances
+    struct user_balance
+    {
+        account_name user;
+        vector<extended_asset> balances;
+
+        auto primary_key() const { return user; }
+    };
+
+    typedef multi_index<N(forsales), forsale> tbl_forsales;
+    typedef multi_index<N(balances), user_balance> tbl_user_balances;
+
+    /* Public interfaces */
+
+    /// @abi action
+    void listforsale(account_name acct, extended_asset price, account_name recipient);
+    /// @abi action
+    void delistacct(account_name acct);
+    /// @abi action
+    void buyacct(account_name buyer, account_name target, extended_asset price, string pub_key);
+    /// @abi action
+    void withdraw(account_name user, extended_asset quantity);
+
+    /* Admin interfaces */
+
+    /// @abi action
+    void adjustfee(uint64_t new_fee);
+    /// @abi action
+    void removesale(account_name acct);
+
+    /* Internal interfaces */
+
+    /// @abi action
+    void assertowner(account_name acct);
+
+    void handle_transfer(account_name from, account_name to, extended_asset quantity, string memo);
+
+  private:
+    tbl_forsales forsales;
+
+    void sub_balance(account_name user, extended_asset quantity);
+};
+
+extern "C"
+{
+    void apply(uint64_t receiver, uint64_t code, uint64_t action)
+    {
+        auto self = receiver;
+        acctexchange thiscontract(self);
+        if (code == self)
+        {
+            switch (action)
+            {
+                EOSIO_API(acctexchange, (listforsale)(delistacct)(adjustfee)(removesale)(assertowner))
+            }
+        }
+        else
+        {
+            if (action == N(transfer))
+            {
+                auto transfer_data = unpack_action_data<transfer_args>();
+                thiscontract.handle_transfer(transfer_data.from, transfer_data.to, extended_asset(transfer_data.quantity, code), transfer_data.memo);
+            }
+        }
+    }
+}
